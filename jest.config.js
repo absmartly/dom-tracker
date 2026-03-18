80b44410f0cb9ad1bc0f9031723fc1dfead9fd98
Jonas Alves <jonas@jonasalves.com>
2026-03-18 12:14:34 +0000

    chore: scaffold @absmartly/dom-tracker project

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts', '!src/index.ts'],
+  coverageThreshold: { global: { branches: 70, functions: 70, lines: 70, statements: 70 } },
+  testMatch: ['**/__tests__/**/*.+(spec|test).+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  transform: { '^.+\\.(ts|tsx)$': 'ts-jest' }
+};
